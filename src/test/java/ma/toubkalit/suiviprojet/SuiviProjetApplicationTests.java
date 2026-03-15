ff1cef401ff8a164231ec93785ff8f1f4ad1e1a0
Khadim1511 <89653168+Khadim1511@users.noreply.github.com>
2026-03-15 02:33:08 +0000

    Add files via upload

---
+++ src/test/java/ma/toubkalit/suiviprojet/SuiviProjetApplicationTests.java
@@ -0,0 +1,8 @@
+package ma.toubkalit.suiviprojet;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class SuiviProjetApplicationTests {
+    @Test
+    void contextLoads() {}
+}
